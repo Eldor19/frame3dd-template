e1b18b4d60866cecd37ba4ff3ed44a4fddcfa175
fassbenderTU <33593884+fassbenderTU@users.noreply.github.com>
2021-12-12 20:24:55 +0100

    als nächstes material properties

--- Frame3DD.gid/Frame3DD.tcl
+++ Frame3DD.gid/Frame3DD.tcl
@@ -430,4 +430,47 @@ proc Frame3DD::TransformResultsToGiD { filename } {
     return
 }
 
+######################################################################
+#  auxiliary procs invoked from the tree (see .spd xml description)
+proc Frame3DD::GetMaterialsList { domNode args } {    
+    set x_path {//container[@n="materials"]}
+    set dom_materials [$domNode selectNodes $x_path]
+    if { $dom_materials == "" } {
+        error [= "xpath '%s' not found in the spd file" $x_path]
+    }
+    set image material
+    set result [list]
+    foreach dom_material [$dom_materials childNodes] {
+        set name [$dom_material @name] 
+        lappend result [list 0 $name $name $image 1]
+    }
+    return [join $result ,]
+}
 
+proc Frame3DD::EditDatabaseList { domNode dict boundary_conds args } {
+    set has_container ""
+    set database materials    
+    set title [= "User defined"]      
+    set list_name [$domNode @n]    
+    set x_path {//container[@n="materials"]}
+    set dom_materials [$domNode selectNodes $x_path]
+    if { $dom_materials == "" } {
+        error [= "xpath '%s' not found in the spd file" $x_path]
+    }
+    set primary_level material
+    if { [dict exists $dict $list_name] } {
+        set xps $x_path
+        append xps [format_xpath {/blockdata[@n=%s and @name=%s]} $primary_level [dict get $dict $list_name]]
+    } else { 
+        set xps "" 
+    }
+    set domNodes [gid_groups_conds::edit_tree_parts_window -accepted_n $primary_level -select_only_one 1 $boundary_conds $title $x_path $xps]          
+    set dict ""
+    if { [llength $domNodes] } {
+        set domNode [lindex $domNodes 0]
+        if { [$domNode @n] == $primary_level } {      
+            dict set dict $list_name [$domNode @name]
+        }
+    }
+    return [list $dict ""]
+}
